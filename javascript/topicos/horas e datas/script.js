3aecda4d2a7af74223cf5a7d271e1d03a81ea077
Bruno e Sousa Alencar <brunoesousaalencar@gmail.com>
2022-07-02 14:48:02 -0300

    Update script.js

--- javascript/topicos/horas e datas/script.js
+++
@@ -1,7 +0,0 @@
-now = new Date
-
-semana = new Array("Domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
-
-mes = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
-
-document.write ("Hoje é " + semana[now.getDay()]+'-feira'  + " do mês " + mes [now.getDay()]+ " de " + now.getFullYear()+ ".")
